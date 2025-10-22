049cf44028cb026b9df81ea251cfc2fbc4d79973
Nguyen Phu Truong <45969976+phutruonnttn@users.noreply.github.com>
2025-10-22 16:39:43 +0800

    [YM-43] Using config server

--- src/test/java/com/yushan/gamification_service/integration/GamificationServiceIntegrationTest.java
+++ src/test/java/com/yushan/gamification_service/integration/GamificationServiceIntegrationTest.java
@@ -2,19 +2,8 @@ package com.yushan.gamification_service.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-        "spring.datasource.url=jdbc:h2:mem:testdb",
-        "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.jpa.hibernate.ddl-auto=create-drop",
-        "spring.flyway.enabled=false",
-        "eureka.client.enabled=false",
-        "spring.data.redis.host=localhost",
-        "spring.data.redis.port=6379",
-        "spring.kafka.bootstrap-servers=localhost:9092"
-})
 class GamificationServiceIntegrationTest {
 
     @Test
